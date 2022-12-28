e2ad7743d423f4bb47f9b42351e311b97630c080
Gorgulho <gorgulho.joao000@gmail.com>
2022-12-28 15:55:13 +0000

    Start

---
+++ client/src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+function Navigation() {
+    return (
+        <div className="App">
+            <Link to={"/coco"}>aaaaaaa</Link>
+        </div>
+    );
+}
+
+export default Navigation;
