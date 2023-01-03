9a1c37bd4c7f2a8a3f78276e4387722e242ac7f1
Ana <a71350@ualg.pt>
2023-01-03 00:10:22 +0000

    menu e receita

---
+++ client/src/components/MyMenu.tsx
@@ -0,0 +1,65 @@
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import React, {useEffect, useState} from "react";
+import '../App.css';
+import {CardActions, Card, CardContent, Typography, IconButton, Container, CardHeader} from "@mui/material";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+function Menu(){
+
+    const [menus, setMenus] = useState([]);
+    const [message, setMessage] = useState("");
+
+    async function fetchData() {
+        const response = await fetch("http://localhost:8080/menus")
+        const json = await response.json()
+        console.log(json)
+        if(json.message) setMessage(json.message)
+        else setMenus(json);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return(
+        <div >
+            <Navigation/>
+
+            {message ? <Container>
+                <Card  sx={{m:3}} style={{boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)"}}>
+                    <CardContent>
+                        <Typography variant="h5">
+                            {message}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Container> : null}
+
+            {menus.map(menu =>
+                <Container>
+                    <Card sx={{mt: 3}} style={{boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)"}}>
+                        <CardHeader
+                            title={menu.date}
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                                {menu.meal} : {menu.recipeName}
+                            </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing style={{display: "flex", justifyContent: "flex-end"}}>
+                            <IconButton aria-label="open">
+                                <OpenInNewIcon/>
+                            </IconButton>
+                            <IconButton aria-label="done">
+                                <TaskAltIcon/>
+                            </IconButton>
+                        </CardActions>
+                    </Card>
+                </Container>
+            )}
+            <Footer/>
+        </div>
+    );
+}
+export default Menu;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import App2 from './App.test'
 import Home from './Home'
 import Create from './components/CreateRecipe'
+import Menu from './components/MyMenu'
+import Recipe from './components/Recipe'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
@@ -16,6 +18,8 @@ root.render(
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/createRecipe" element={<Create />} />
+              <Route path="/myMenu" element={<Menu />} />
+              <Route path="/recipe" element={<Recipe />}/>
               <Route path="/test" element={<App />}/>
           </Routes>
       </Router>
