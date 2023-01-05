4739c07324406e90c12d08b03172d6ba8ec1ea56
Gorgulho <gorgulho.joao000@gmail.com>
2023-01-05 21:16:07 +0000

    Documentation added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './css/index.css';
+import 'normalize.css'
 import Create from './components/CreateRecipe'
 import Menu from './components/MyMenu'
 import Recipe from './components/Recipe'
@@ -11,6 +12,10 @@ import Recipes from "./components/Recipes";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+/**
+ * Creation of all the Routes needed with the help of the react-router-dom.
+ * The react-router-dom is a popular library that provides routing functionality for React applications.
+ * */
 root.render(
   <React.StrictMode>
       <Router>
